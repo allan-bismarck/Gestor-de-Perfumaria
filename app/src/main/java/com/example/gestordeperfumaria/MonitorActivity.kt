2b322533ab41a8bfb9b6913fd6b03c34e4974cb9
AllanBismarck123 <allanrock60@hotmail.com>
2023-04-13 00:15:37 -0300

    view pager

--- app/src/main/java/com/example/gestordeperfumaria/MonitorActivity.kt
+++ app/src/main/java/com/example/gestordeperfumaria/MonitorActivity.kt
@@ -4,59 +4,203 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gestordeperfumaria.databinding.ActivityMonitorBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MonitorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMonitorBinding
+    private lateinit var months: List<String>
+    private lateinit var cosmetics: MutableList<CosmeticEntity>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMonitorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        initListMonths()
+
+        val adapter = ViewPagerAdapter(this, months, listOf(CosmeticEntity(0,"",0,"",0f,false)))
         binding.pager.adapter = adapter
+        binding.pager.currentItem = months.lastIndex
+    }
 
-        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            when(position) {
-                0 -> {
-                    tab.text = "Janeiro"
-                }
-                1 -> {
-                    tab.text = "Fevereiro"
-                }
-                2 -> {
-                    tab.text = "Março"
-                }
-                3 -> {
-                    tab.text = "Abril"
-                }
-                4 -> {
-                    tab.text = "Maio"
-                }
-                5 -> {
-                    tab.text = "Junho"
-                }
-                6 -> {
-                    tab.text = "Julho"
-                }
-                7 -> {
-                    tab.text = "Agosto"
-                }
-                8 -> {
-                    tab.text = "Setembro"
-                }
-                9 -> {
-                    tab.text = "Outubro"
-                }
-                10 -> {
-                    tab.text = "Novembro"
-                }
-                else -> {
-                    tab.text = "Dezembro"
-                }
-            }
-        }.attach()
+    private fun initListMonths() {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        val date = dateFormat.format(Date()).toString()
+        val dateSplit = date.split('/')
+        val year = dateSplit[2].toInt()
+        val month = dateSplit[1].toInt()
 
+        when(month) {
+            1 -> months = listOf(
+                "Fevereiro/${year-1}",
+                "Março/${year-1}",
+                "Abril/${year-1}",
+                "Maio/${year-1}",
+                "Junho/${year-1}",
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+            )
+            2 -> months = listOf(
+                "Março/${year-1}",
+                "Abril/${year-1}",
+                "Maio/${year-1}",
+                "Junho/${year-1}",
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+            )
+            3 -> months = listOf(
+                "Abril/${year-1}",
+                "Maio/${year-1}",
+                "Junho/${year-1}",
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+            )
+            4 -> months = listOf(
+                "Maio/${year-1}",
+                "Junho/${year-1}",
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+            )
+            5 -> months = listOf(
+                "Junho/${year-1}",
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+            )
+            6 -> months = listOf(
+                "Julho/${year-1}",
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+            )
+            7 -> months = listOf(
+                "Agosto/${year-1}",
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+            )
+            8 -> months = listOf(
+                "Setembro/${year-1}",
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+                "Agosto/${year}",
+            )
+            9 -> months = listOf(
+                "Outubro/${year-1}",
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+                "Agosto/${year}",
+                "Setembro/${year}",
+            )
+            10 -> months = listOf(
+                "Novembro/${year-1}",
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+                "Agosto/${year}",
+                "Setembro/${year}",
+                "Outubro/${year}",
+            )
+            11 -> months = listOf(
+                "Dezembro/${year-1}",
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+                "Agosto/${year}",
+                "Setembro/${year}",
+                "Outubro/${year}",
+                "Novembro/${year}",
+            )
+            else -> months = listOf(
+                "Janeiro/${year}",
+                "Fevereiro/${year}",
+                "Março/${year}",
+                "Abril/${year}",
+                "Maio/${year}",
+                "Junho/${year}",
+                "Julho/${year}",
+                "Agosto/${year}",
+                "Setembro/${year}",
+                "Outubro/${year}",
+                "Novembro/${year}",
+                "Dezembro/${year}",
+            )
+        }
     }
 }
--- app/src/main/java/com/example/gestordeperfumaria/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/gestordeperfumaria/ViewPagerAdapter.kt
@@ -1,65 +1,51 @@
 package com.example.gestordeperfumaria
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gestordeperfumaria.databinding.ItemViewPagerBinding
 
-class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
-    override fun getItemCount(): Int {
-        return 12
+class ViewPagerAdapter(
+    private val context: Context,
+    val months: List<String>,
+    val cosmeticsProfit: List<CosmeticEntity>
+): RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+
+    inner class ViewPagerViewHolder(binding: ItemViewPagerBinding): RecyclerView.ViewHolder(binding.root) {
+        var month = binding.month
+        var profitContent = binding.profitContent
+        var expenseContent = binding.expenseContent
+        var totalContent = binding.totalContent
+        val btnBack = binding.btnBack
+        val btnNext = binding.btnNext
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return when(position) {
-            0 -> {
-                MonitorFragment.cOText = "1"
-                MonitorFragment()
-            }
-            1 -> {
-                MonitorFragment.cOText = "2"
-                MonitorFragment()
-            }
-            2 -> {
-                MonitorFragment.cOText = "3"
-                MonitorFragment()
-            }
-            3 -> {
-                MonitorFragment.cOText = "4"
-                MonitorFragment()
-            }
-            4 -> {
-                MonitorFragment.cOText = "5"
-                MonitorFragment()
-            }
-            5 -> {
-                MonitorFragment.cOText = "6"
-                MonitorFragment()
-            }
-            6 -> {
-                MonitorFragment.cOText = "7"
-                MonitorFragment()
-            }
-            7 -> {
-                MonitorFragment.cOText = "8"
-                MonitorFragment()
-            }
-            8 -> {
-                MonitorFragment.cOText = "9"
-                MonitorFragment()
-            }
-            9 -> {
-                MonitorFragment.cOText = "10"
-                MonitorFragment()
-            }
-            10 -> {
-                MonitorFragment.cOText = "11"
-                MonitorFragment()
-            }
-            else ->  {
-                MonitorFragment.cOText = "12"
-                MonitorFragment()
-            }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = ItemViewPagerBinding.inflate(LayoutInflater.from(context), parent, false)
+
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        holder.month.text = months[position]
+        holder.profitContent.text = months[position]
+
+        holder.btnBack.visibility = View.VISIBLE
+        holder.btnNext.visibility = View.VISIBLE
+
+        if(position == 0) {
+            holder.btnBack.visibility = View.INVISIBLE
+        }
+
+        if(position == months.size - 1) {
+            holder.btnNext.visibility = View.INVISIBLE
         }
     }
+
+    override fun getItemCount(): Int {
+        return months.size
+    }
+
 }
