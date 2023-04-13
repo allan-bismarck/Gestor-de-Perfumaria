17ea596adcbb01ce469d66e46330e00b0c3e2e4b
AllanBismarck123 <allanrock60@hotmail.com>
2023-04-13 17:30:22 -0300

    show values in recycler view monitor activity

--- app/src/main/java/com/example/gestordeperfumaria/MonitorActivity.kt
+++ app/src/main/java/com/example/gestordeperfumaria/MonitorActivity.kt
@@ -2,35 +2,52 @@ package com.example.gestordeperfumaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
 import com.example.gestordeperfumaria.databinding.ActivityMonitorBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MonitorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMonitorBinding
+    private lateinit var db: AppDataBase
     private lateinit var months: List<String>
-    private lateinit var cosmetics: MutableList<CosmeticEntity>
+    private lateinit var cosmetics: List<CosmeticEntity>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMonitorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initListMonths()
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDataBase::class.java,
+            "db-perfumery"
+        ).allowMainThreadQueries().build()
 
-        val adapter = ViewPagerAdapter(this, months, listOf(CosmeticEntity(0,"",0,"",0f,false)))
+        initLists()
+
+        val adapter = ViewPagerAdapter(this, months, cosmetics)
         binding.pager.adapter = adapter
         binding.pager.currentItem = months.lastIndex
     }
 
-    private fun initListMonths() {
+    private fun initLists() {
         val dateFormat = SimpleDateFormat("dd/MM/yyyy")
         val date = dateFormat.format(Date()).toString()
         val dateSplit = date.split('/')
         val year = dateSplit[2].toInt()
         val month = dateSplit[1].toInt()
+
+        initCosmetics()
+        initListMonths(month, year)
+    }
+
+    private fun initListMonths(month: Int, year: Int) {
 
         when(month) {
             1 -> months = listOf(
@@ -203,4 +220,16 @@ class MonitorActivity : AppCompatActivity() {
             )
         }
     }
+
+    private fun initCosmetics() {
+        runBlocking {
+            cosmetics = dbShowAllCosmetics()
+        }
+    }
+
+    private suspend fun dbShowAllCosmetics() = runBlocking {
+        var cosmesticList = async { db.cosmeticDAO.getAll() }
+        Log.i("cosmeticList", cosmesticList.await().toString())
+        return@runBlocking cosmesticList
+    }.await()
 }
--- app/src/main/java/com/example/gestordeperfumaria/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/gestordeperfumaria/ViewPagerAdapter.kt
@@ -1,11 +1,19 @@
 package com.example.gestordeperfumaria
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.gestordeperfumaria.databinding.ItemViewPagerBinding
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import java.text.ParsePosition
+import java.text.SimpleDateFormat
+import kotlin.math.exp
 
 class ViewPagerAdapter(
     private val context: Context,
@@ -13,6 +21,7 @@ class ViewPagerAdapter(
     val cosmeticsProfit: List<CosmeticEntity>
 ): RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
 
+    private lateinit var cosmeticListByDate: List<CosmeticEntity>
     inner class ViewPagerViewHolder(binding: ItemViewPagerBinding): RecyclerView.ViewHolder(binding.root) {
         var month = binding.month
         var profitContent = binding.profitContent
@@ -29,8 +38,26 @@ class ViewPagerAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        cosmeticListByDate = getCosmeticsByDate(position)
+        var sales = 0.0f
+        var expenses = 0.0f
+        var total = 0.0f
+
+        cosmeticListByDate.forEach {
+            if(it.isSale) {
+                sales += it.price
+            } else {
+                expenses += it.price
+            }
+        }
+
+        total = sales - expenses
+
+        holder.profitContent.text = "R$${sales}"
+        holder.expenseContent.text = "R$${expenses}"
+        holder.totalContent.text = "R$${total}"
+
         holder.month.text = months[position]
-        holder.profitContent.text = months[position]
 
         holder.btnBack.visibility = View.VISIBLE
         holder.btnNext.visibility = View.VISIBLE
@@ -48,4 +75,34 @@ class ViewPagerAdapter(
         return months.size
     }
 
+    private fun getCosmeticsByDate(position: Int): MutableList<CosmeticEntity> {
+        val cosmeticsListByDate: MutableList<CosmeticEntity> = mutableListOf()
+        cosmeticsProfit.forEach {
+            val dateSplit = it.date.split('/')
+            val datePage = dateToNumberDate(position)
+            if(dateSplit[1].toInt() == datePage.split('/')[0].toInt()) {
+                cosmeticsListByDate.add(it)
+            }
+        }
+        return cosmeticsListByDate
+    }
+
+    private fun dateToNumberDate(position: Int): String {
+        val year = months[position].split('/')[1]
+        val month = months[position].split('/')[0]
+        return when(month) {
+            "Janeiro" -> "1/$year"
+            "Fevereiro" -> "2/$year"
+            "Março" -> "3/$year"
+            "Abril" -> "4/$year"
+            "Maio" -> "5/$year"
+            "Junho" -> "6/$year"
+            "Julho" -> "7/$year"
+            "Agosto" -> "8/$year"
+            "Setembro" -> "9/$year"
+            "Outubro" -> "10/$year"
+            "Novembro" -> "11/$year"
+            else -> "12/$year"
+        }
+    }
 }
